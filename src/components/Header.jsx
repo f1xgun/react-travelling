68cc4ac4392e33f47c56bef1441fcc100bb2f02e
Sergey Yaksanov <iaksanov2044@mail.ru>
2021-10-12 21:12:19 +0400

    Added news slider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,23 @@
-import Button from './Button'
-
-function Header () {
+import Button from "./Button";
+import styles from "./Header.module.scss";
+function Header() {
   return (
-    <header>
-      <div className="header__logo">
-        <img src="/img/logo.svg" className="header__logo-img" alt="Logo"></img>
-        <h2 className="header__logo-title">Way<span>farer</span></h2>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src="/img/logo.svg" alt="Logo" />
+        <h2>
+          Way<span>farer</span>
+        </h2>
       </div>
-      <ul className="header__links">
-        <li className="active">Home</li>
+      <ul className={styles.links}>
+        <li className={styles.active}>Home</li>
         <li>Place</li>
         <li>News</li>
         <li>Feedback</li>
       </ul>
-      <Button className="header__login" title="Login"/>
+      <Button className={styles.login} title="Login" />
     </header>
-  )
+  );
 }
 
 export default Header;
