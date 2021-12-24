1105e47564bce9746dfccbf5a0e3ce7e5b7cc2fa
f1xgun <msnzlmm@mail.ru>
2021-12-24 19:16:13 +0400

    update

--- server/index.js
+++ server/index.js
@@ -13,18 +13,21 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, '/client/build')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 
 
 // Обработка ошибок, последний Middleware
 app.use(errorHandler)
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+}
 
 
 const start = async () => {
